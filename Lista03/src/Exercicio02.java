140393ef1f6664eb6185e88a96a8d22c5561dcc9
NaaQuinteiro <thaynaquinteiro@gmail.com>
2024-03-26 14:24:47 -0300

    Listas Revisao

--- Lista03/src/Exercicio02.java
+++ Lista03/src/Exercicio02.java
@@ -1,10 +1,11 @@
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Exercicio01 {
+public class Exercicio02 {
     public static void main(String[] args) {
 
-        int numero, pares = 0, impares = 0, negativos = 0, positivos = 0;
+        double altura, peso;
+        int idade, acima50 = 0, acima160 = 0, abaixo80 = 0;
         int contador = 1, opcaoUsuario;
 
         Scanner read = new Scanner(System.in);
@@ -13,28 +14,33 @@ public class Exercicio01 {
 
             while (true){
                 try {
-                    System.out.printf("\nInsira o %d° numero: ", contador);
-                    numero = read.nextInt();
+                    System.out.printf("\nInsira a idade da %d° pessoa: ", contador);
+                    idade = read.nextInt();
+
+                    System.out.printf("\nInsira o peso da %d° pessoa: ", contador);
+                    peso = read.nextDouble();
+
+                    System.out.printf("\nInsira a altura da %d° pessoa: ", contador);
+                    altura = read.nextDouble();
                     break;
+
                 }catch (InputMismatchException e){
                     System.out.println("ERRO: Ops.. você inseriu caracteres, por favor tente novamente com números inteiros");
                 }
                 read.nextLine();
             }
 
-            if(numero %2 == 0){
-                pares = pares +1;
+            if(idade > 50){
+                acima50 = acima50 +1;
 
             }
-            else{
-                impares = impares +1;
+
+            if(peso < 80){
+                abaixo80 = abaixo80 +1;
             }
 
-            if(numero < 0){
-                negativos = negativos +1;
-            }
-            else{
-                positivos = positivos +1;
+            if(altura > 1.60){
+                acima160 = acima160 +1;
             }
 
             contador = contador +1;
@@ -42,40 +48,34 @@ public class Exercicio01 {
         }while (contador <= 10);
 
 
-
         while (true){
             try {
                 System.out.println("\nO que você deseja ver:\n" +
-                            "1. Quantos números pares?\n" +
-                            "2. Quantos números ímpares?\n" +
-                            "3. Quantos negativos?\n" +
-                            "4. Quantos positivos?\n" +
-                            "5. Sair do programa");
+                        "1. Quantas tem idade acima de 50 anos\n" +
+                        "2. Quantas tem altura acima de 1.60m\n" +
+                        "3. Quantas tem peso abaixo de 80kg\n" +
+                        "4. Sair do programa");
                 opcaoUsuario = read.nextInt();
 
                 switch (opcaoUsuario){
                     case 1: {
-                        System.out.printf("\nPares: %d", pares);
+                        System.out.printf("\nQuantidade de pessoas com idade acima de 50 anos: %d\n", acima50);
                         continue;
                     }
                     case 2: {
-                        System.out.printf("\nÍmpares: %d", impares);
+                        System.out.printf("\nQuantidade de pessoas com altura acima de 1.60m: %d\n", acima160);
                         continue;
                     }
                     case 3: {
-                        System.out.printf("\nNegativos: %d", negativos);
+                        System.out.printf("\nQuantidade de pessoas com peso abaixo de 80kg: %d\n", abaixo80);
                         continue;
                     }
-                    case 4: {
-                        System.out.printf("\nPositivos: %d", positivos);
-                        continue;
-                    }
-                    case 5:{
+                    case 4:{
                         System.out.print("\nObrigada por usar nosso programa! Até logo =)");
                         System.exit(0);
                     }
                     default:{
-                        System.out.println("\nOpção inválida, tente novamente");
+                        System.out.println("\nOpção inválida, tente novamente\n");
                         continue;
                     }
                 }
@@ -84,6 +84,5 @@ public class Exercicio01 {
             }
             read.nextLine();
         }
-
     }
 }
